ce9a294205489adf4f1962a31e9cf574d4228bf2
Anshu Bhandari <anshuuuuu112@gmail.com>
2026-08-01 20:29:15 +0545

    Added new menu items and images

--- script.js
+++ script.js
@@ -1,24 +1,56 @@
-document.getElementById("reservationForm").addEventListener("submit", function(event){
 
-    event.preventDefault();
+function showMenu(category){
 
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let phone = document.getElementById("phone").value;
+    let menus = document.querySelectorAll(".menu-items");
 
-    let message = document.getElementById("message");
+    menus.forEach(function(menu){
 
-    if(name === "" || email === "" || phone === "")
-    {
-        message.innerHTML = "Please fill all the details!";
-        message.style.color = "red";
+        menu.style.display = "none";
+
+    });
+
+    document.getElementById(category).style.display = "block";
+
+}
+
+// Show Pizza by default
+window.onload = function(){
+
+    if(document.getElementById("pizza")){
+        showMenu("pizza");
     }
-    else
-    {
-        message.innerHTML = "Thank you " + name + "! Your table has been reserved successfully.";
-        message.style.color = "green";
 
-        document.getElementById("reservationForm").reset();
-    }
+};
 
-});
+const reservationForm = document.getElementById("reservationForm");
+
+if(reservationForm){
+
+    reservationForm.addEventListener("submit", function(event){
+
+        event.preventDefault();
+
+        let name = document.getElementById("name").value;
+        let email = document.getElementById("email").value;
+        let phone = document.getElementById("phone").value;
+        let message = document.getElementById("message");
+
+        if(name === "" || email === "" || phone === ""){
+
+            message.innerHTML = "Please fill all the details.";
+            message.style.color = "red";
+
+        }
+
+        else{
+
+            message.innerHTML = "Thank you " + name + "! Your table has been reserved successfully.";
+            message.style.color = "green";
+
+            reservationForm.reset();
+
+        }
+
+    });
+
+}
